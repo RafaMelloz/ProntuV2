903dbfb0289e49b90ba4cfe1f1f4a115aee56b6a
RafaMelloz <rafaelmeloalvessouza@gmail.com>
2024-09-12 12:11:12 -0300

    trocando biblioteca de criptografia ajuste 1

--- src/app/api/accesses/route.js
+++ src/app/api/accesses/route.js
@@ -5,7 +5,8 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import prisma from "@/lib/prisma";
 import cloudinary from 'cloudinary';
-import { hash } from "crypto";
+import { hash } from "bcrypt";
+
 
 
 cloudinary.config({
